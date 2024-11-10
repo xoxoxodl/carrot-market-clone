e6f5555b44b0c49856237588f0986dfe75b24c19
xoxoxodl <tayeeekim@gmail.com>
2024-11-10 17:12:54 -0500

    login page

--- main.py
+++ main.py
@@ -61,6 +61,20 @@ async def get_image(item_id):
                              SELECT image from items WHERE id = {item_id}
                              """).fetchone()[0]
     
-    return Response(content=bytes.fromhex(image_bytes))
+    return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
+
+
+
+@app.post('/signup')
+def signup(id:Annotated[str, Form()], 
+           password:Annotated[str, Form()],
+           name:Annotated[str, Form()],
+           email:Annotated[str, Form()]):
+    cur.execute(f"""
+                INSERT INTO users(id, name, email, password)
+                VALUES ('{id}','{name}', '{email}', '{password}')
+                """)
+    con.commit()
+    return '200'
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
